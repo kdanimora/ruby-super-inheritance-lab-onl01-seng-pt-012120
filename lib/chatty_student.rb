068fc4e6719374420fb9e5578581eec36bca6051
Karen Daniela <kdaniimora@gmail.com>
2020-02-23 11:36:58 +0000

    Automatically backed up by Learn

--- lib/chatty_student.rb
+++ lib/chatty_student.rb
@@ -1,5 +1,10 @@
 class Chatty_Student < Student 
   
+  def initialize(hello, raise_hand)
+    @hello = hello
+    @raise_hand = raise_hand 
+end 
+  
   def hello 
     super 
    puts  "How are you doing today? I'm okay, but I'm kind of tired. Did you watch The Walking Dead last night? You didn't?! Oh man, it was so crazy! What, you don't want any spoilers? Okay well let me just tell you who died..."
